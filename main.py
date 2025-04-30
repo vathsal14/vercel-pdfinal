957058d740e51a0ed7e5afb966a94ca2c682bf2c
vathsal14 <150268447+vathsal14@users.noreply.github.com>
2025-04-30 16:07:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # For production, replace with your Vercel frontend URL
+    allow_origins=["https://parkinsons-prediction-git-main-vathsal14-gmailcoms-projects.vercel.app/", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(predict_router, prefix="/api")
 
